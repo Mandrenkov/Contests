cd28785b58ecf3622573b6d14ec0e1b534992a64
Mandrenkov <M.Andrenkov@gmail.com>
2020-11-30 18:51:04 -0500

    Added solutions to Kick Start 2013 Practice Round.

---
+++ Kick Start 2013/Practice Round/C.cpp
@@ -0,0 +1,172 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Common Utilities
+//______________________________________________________________________________
+
+// Macros
+#ifdef D3BUG
+    #define cerrd if (D3BUG) cerr
+#else
+    #define cerrd if (false) cerr
+#endif
+
+// Types
+template <typename T> using vector1D = vector<T>;
+template <typename T> using vector2D = vector<vector1D<T>>;
+template <typename T> using vector3D = vector<vector2D<T>>;
+template <typename T> using uset = unordered_set<T>;
+template <typename K, typename V> using umap = unordered_map<K, V>;
+
+// Output stream overloads.
+template <typename T>
+ostream& operator<<(ostream& out, const vector<T>& v) {
+    out << '[';
+    for (int i = 0; i < v.size(); ++i) {
+        out << v[i];
+        if (i < v.size() - 1) {
+            out << ", ";
+        }
+    }
+    return out << ']';
+}
+
+template <typename T>
+ostream& operator<<(ostream& out, const deque<T>& d) {
+    if (d.size() == 0) {
+        return out << "[]";
+    }
+
+    out << '[' << d[0];
+    for (int i = 1; i < d.size(); ++i) {
+        out << ' ' << d[i];
+    }
+    return out << ']';
+}
+
+template <typename T>
+ostream& operator<<(ostream& out, const set<T>& s) {
+    if (s.size() == 0) {
+        return out << "{}";
+    }
+
+    out << '{' << *s.begin();
+    for (auto it = ++s.begin(); it != s.end(); ++it) {
+        out << ' ' << *it;
+    }
+    return out << '}';
+}
+
+template <typename T>
+ostream& operator<<(ostream& out, const multiset<T>& s) {
+    return out << vector<T>(s.begin(), s.end());
+}
+
+template <typename T>
+ostream& operator<<(ostream& out, const unordered_set<T>& s) {
+    if (s.size() == 0) {
+        return out << "{}";
+    }
+
+    vector<T> v(s.begin(), s.end());
+    sort(v.begin(), v.end());
+
+    out << '{' << v[0];
+    for (int i = 1; i < v.size(); ++i) {
+        out << ' ' << v[i];
+    }
+    return out << '}';
+}
+
+template <typename K, typename V>
+ostream& operator<<(ostream& out, const map<K, V>& m) {
+    if (m.size() == 0) {
+        return out << "{}";
+    }
+
+    out << "{\n";
+    for (const auto& p : m) {
+        const K& key = p.first;
+        out << "    " << key << ": " << m.at(key) << '\n';
+    }
+    return out << '}';
+}
+
+template <typename K, typename V>
+ostream& operator<<(ostream& out, const unordered_map<K, V>& m) {
+    if (m.size() == 0) {
+        return out << "{}";
+    }
+
+    vector<K> keys;
+    for (const auto& p : m) {
+        keys.push_back(p.first);
+    }
+    sort(keys.begin(), keys.end());
+
+    out << "{\n";
+    for (const auto& key : keys) {
+        out << "    " << key << ": " << m.at(key) << '\n';
+    }
+    return out << '}';
+}
+
+template <typename F, typename S>
+ostream& operator<<(ostream& out, const pair<F, S>& p) {
+    return out << '(' << p.first << ", " << p.second << ')';
+}
+
+template<typename T, typename U>
+struct hash<pair<T, U>> {
+    size_t operator()(const pair<T, U>& p) const noexcept {
+        return hash<T>{}(p.first) ^ (hash<U>{}(p.second) << 1);
+    }
+};
+
+void solution();
+
+// Initalize the execution environment and call the solution function.
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    solution();
+    return 0;
+}
+
+// Solution
+// _____________________________________________________________________________
+
+void solution() {
+    int T;
+    cin >> T;
+    for (int t = 1; t <= T; ++t) {
+        int N;
+        cin >> N;
+        unordered_map<string, vector<string>> G;
+        for (int i = 0; i < N; ++i) {
+            string name1, name2;
+            cin >> name1 >> name2;
+            G[name1].emplace_back(name2);
+            G[name2].emplace_back(name1);
+        }
+        unordered_map<string, int> C;
+        const function<bool(const string&, int)> explore = [&](const string& node, int colour) {
+            if (C[node] == 0) {
+                C[node] = colour;
+                for (const auto& next : G[node]) {
+                    if (!explore(next, colour == 1 ? 2 : 1)) {
+                        return false;
+                    }
+                }
+                return true;
+            } else if (C[node] != colour) {
+                return false;
+            } else {
+                return true;
+            }
+        };
+        bool ok = explore(G.begin()->first, 1);
+        cout << "Case #" << t << ": " << (ok ? "Yes" : "No") << '\n';
+    }
+}
