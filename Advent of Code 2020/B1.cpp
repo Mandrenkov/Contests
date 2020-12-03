c19d21c3ff5af09a82d2c6b2762ff202455f78d7
Mandrenkov <M.Andrenkov@gmail.com>
2020-12-03 11:44:52 -0500

    Added solutions to AoC 2020 Day 2 (B).

---
+++ Advent of Code 2020/B1.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Macros
+#ifdef D3BUG
+    #define cerrd if (D3BUG) cerr
+#else
+    #define cerrd if (false) cerr
+#endif
+
+// Types
+template <typename T> using vector1D = vector<T>;
+template <typename T> using vector2D = vector<vector1D<T>>;
+template <typename T> using vector3D = vector<vector2D<T>>;
+
+// Output Stream Overloads
+template<
+    // Multiple template arguments may be needed for containers like std::map.
+    template<class...> class Range, class... Ts,
+    // This overload must be disabled for std::string to avoid ambiguity.
+    typename = std::enable_if_t<!std::is_same_v<Range<Ts...>, std::string>>
+>
+std::ostream& operator<<(std::ostream& out, const Range<Ts...>& range) {
+    out << '{';
+    for (auto it = range.begin(); it != range.end(); ++it) {
+        out << *it << (std::next(it) != range.end() ? ", " : "");
+    }
+    return out << '}';
+}
+
+template <class F, class S>
+ostream& operator<<(ostream& out, const pair<F, S>& pair) {
+    return out << '(' << pair.first << ", " << pair.second << ')';
+}
+
+// Hash Template Specializations
+template<class T, class U>
+struct hash<pair<T, U>> {
+    size_t operator()(const pair<T, U>& p) const noexcept {
+        return hash<T>{}(p.first) ^ (hash<U>{}(p.second) << 1);
+    }
+};
+
+void solution();
+
+// Initalize the execution environment and call the solution function.
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    solution();
+}
+
+// -----------------------------------------------------------------------------
+
+void solution() {
+    int N;
+    cin >> N;
+    int valid = 0;
+    for (int i = 0; i < N; ++i) {
+        string range;
+        cin >> range;
+        int lo = stoi(range.substr(0, range.find_first_of('-')));
+        int hi = stoi(range.substr(range.find_first_of('-') + 1));
+
+        char letter, colon;
+        cin >> letter >> colon;
+
+        string password;
+        cin >> password;
+
+        int frequency = count(password.begin(), password.end(), letter);
+        valid += lo <= frequency && frequency <= hi;
+    }
+    cout << valid << '\n';
+}
