558af9643b8b36304f5f77f59f2856796cc83d8e
Mandrenkov <M.Andrenkov@gmail.com>
2020-12-03 11:45:04 -0500

    Added solutions to AoC 2020 Day 3 (C).

---
+++ Advent of Code 2020/C1.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Macros
+#ifdef D3BUG
+    #define cerrd if (D3BUG) cerr
+#else
+    #define cerrd if (false) cerr
+#endif
+
+// Types
+template <typename T> using vector1D = vector<T>;
+template <typename T> using vector2D = vector<vector1D<T>>;
+template <typename T> using vector3D = vector<vector2D<T>>;
+
+// Output Stream Overloads
+template<
+    // Multiple template arguments may be needed for containers like std::map.
+    template<class...> class Range, class... Ts,
+    // This overload must be disabled for std::string to avoid ambiguity.
+    typename = std::enable_if_t<!std::is_same_v<Range<Ts...>, std::string>>
+>
+std::ostream& operator<<(std::ostream& out, const Range<Ts...>& range) {
+    out << '{';
+    for (auto it = range.begin(); it != range.end(); ++it) {
+        out << *it << (std::next(it) != range.end() ? ", " : "");
+    }
+    return out << '}';
+}
+
+template <class F, class S>
+std::ostream& operator<<(std::ostream& out, const std::pair<F, S>& pair) {
+    return out << '(' << pair.first << ", " << pair.second << ')';
+}
+
+// Hash Template Specializations
+template<class T, class U>
+struct hash<std::pair<T, U>> {
+    size_t operator()(const std::pair<T, U>& pair) const noexcept {
+        return std::hash<T>{}(pair.first) ^ (std::hash<U>{}(pair.second) << 1);
+    }
+};
+
+void solution();
+
+// Initalize the execution environment and call the solution function.
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    solution();
+}
+
+// -----------------------------------------------------------------------------
+
+void solution() {
+    int R, C;
+    cin >> R >> C;
+    vector<string> forest(R);
+    for (int i = 0; i < R; ++i) {
+        cin >> forest[i];
+    }
+    int trees = 0;
+    for (int r = 0, c = 0; r < R; c = (c + 3) % C, ++r) {
+        trees += forest[r][c] == '#';
+    }
+    cout << trees << '\n';
+}
