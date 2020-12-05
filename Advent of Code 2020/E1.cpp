af2c17c7ea7582c2285d0cbf731a0b8299a375c5
Mandrenkov <M.Andrenkov@gmail.com>
2020-12-05 12:33:28 -0500

    Added solutions to Advent of Code 2020 Day 5 (E).

---
+++ Advent of Code 2020/E1.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Macros
+#ifdef D3BUG
+    #define cerrd if (D3BUG) cerr
+#else
+    #define cerrd if (false) cerr
+#endif
+
+// Types
+template <typename T> using vector1D = vector<T>;
+template <typename T> using vector2D = vector<vector1D<T>>;
+template <typename T> using vector3D = vector<vector2D<T>>;
+
+// Output Stream Overloads
+template<
+    // Multiple template arguments may be needed for containers like std::map.
+    template<class...> class Range, class... Ts,
+    // This overload must be disabled for std::string to avoid ambiguity.
+    typename = std::enable_if_t<!std::is_same_v<Range<Ts...>, std::string>>
+>
+std::ostream& operator<<(std::ostream& out, const Range<Ts...>& range) {
+    out << '{';
+    for (auto it = range.begin(); it != range.end(); ++it) {
+        out << *it << (std::next(it) != range.end() ? ", " : "");
+    }
+    return out << '}';
+}
+
+template <class F, class S>
+std::ostream& operator<<(std::ostream& out, const std::pair<F, S>& pair) {
+    return out << '(' << pair.first << ", " << pair.second << ')';
+}
+
+// Hash Template Specializations
+template<class T, class U>
+struct hash<std::pair<T, U>> {
+    size_t operator()(const std::pair<T, U>& pair) const noexcept {
+        return std::hash<T>{}(pair.first) ^ (std::hash<U>{}(pair.second) << 1);
+    }
+};
+
+void solution();
+
+// Initalize the execution environment and call the solution function.
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    solution();
+}
+
+// -----------------------------------------------------------------------------
+
+// Returns the ID of the given boarding pass.
+int identify(const string& pass) {
+    int id = 0;
+    for (int i = 0; i < 10; ++i) {
+        id = (id << 1) | (pass[i] == 'B' || pass[i] == 'R');
+    }
+    return id;
+}
+
+void solution() {
+    int N;
+    cin >> N;
+    int highest = 0;
+    for (int i = 0; i < N; ++i) {
+        string pass;
+        cin >> pass;
+        int id = identify(pass);
+        highest = max(highest, id);
+    }
+    cout << highest << '\n';
+}
