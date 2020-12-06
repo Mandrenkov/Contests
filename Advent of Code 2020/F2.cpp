cf141d6070023d3554230d1b04ba13d6a576ec3a
Mandrenkov <M.Andrenkov@gmail.com>
2020-12-06 14:19:37 -0500

    Added solutions to Advent of Code 2020 Day 6 (F).

---
+++ Advent of Code 2020/F2.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Macros
+#ifdef D3BUG
+    #define cerrd if (D3BUG) cerr
+#else
+    #define cerrd if (false) cerr
+#endif
+
+// Types
+template <typename T> using vector1D = vector<T>;
+template <typename T> using vector2D = vector<vector1D<T>>;
+template <typename T> using vector3D = vector<vector2D<T>>;
+
+// Output Stream Overloads
+template<
+    // Multiple template arguments may be needed for containers like std::map.
+    template<class...> class Range, class... Ts,
+    // This overload must be disabled for std::string to avoid ambiguity.
+    typename = std::enable_if_t<!std::is_same_v<Range<Ts...>, std::string>>
+>
+std::ostream& operator<<(std::ostream& out, const Range<Ts...>& range) {
+    out << '{';
+    for (auto it = range.begin(); it != range.end(); ++it) {
+        out << *it << (std::next(it) != range.end() ? ", " : "");
+    }
+    return out << '}';
+}
+
+template <class F, class S>
+std::ostream& operator<<(std::ostream& out, const std::pair<F, S>& pair) {
+    return out << '(' << pair.first << ", " << pair.second << ')';
+}
+
+// Hash Template Specializations
+template<class T, class U>
+struct hash<std::pair<T, U>> {
+    size_t operator()(const std::pair<T, U>& pair) const noexcept {
+        return std::hash<T>{}(pair.first) ^ (std::hash<U>{}(pair.second) << 1);
+    }
+};
+
+void solution();
+
+// Initalize the execution environment and call the solution function.
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    solution();
+}
+
+// -----------------------------------------------------------------------------
+
+void solution() {
+    string line;
+    getline(cin, line);
+    int N = stoi(line);
+    int counter = 0;
+    for (int i = 0; i < N; ++i) {
+        bitset<26> questions = 0x3FFFFFF;
+        while (getline(cin, line) && !line.empty()) {
+            bitset<26> answers;
+            for (const char answer : line) {
+                answers |= 1 << (answer - 'a');
+            }
+            questions &= answers;
+        }
+        counter += questions.count();
+    }
+    cout << counter << '\n';
+}
