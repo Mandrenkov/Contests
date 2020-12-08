3a4a8b12f126c3699f55e2ae4335d2df43d88063
Mandrenkov <M.Andrenkov@gmail.com>
2020-12-07 19:48:42 -0500

    Added solutions to Advent of Code 2020 Day 7 (G).

---
+++ Advent of Code 2020/G2.cpp
@@ -0,0 +1,123 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Macros
+#ifdef D3BUG
+    #define cerrd if (D3BUG) cerr
+#else
+    #define cerrd if (false) cerr
+#endif
+
+// Types
+template <typename T> using vector1D = vector<T>;
+template <typename T> using vector2D = vector<vector1D<T>>;
+template <typename T> using vector3D = vector<vector2D<T>>;
+
+template<
+    // Output stream overload for STL containers with one explicit type parameter.
+    template<class> class Range, class T,
+    typename = enable_if_t<is_same_v<Range<T>, deque<T>> ||
+                           is_same_v<Range<T>, forward_list<T>> ||
+                           is_same_v<Range<T>, list<T>> ||
+                           is_same_v<Range<T>, multiset<T>> ||
+                           is_same_v<Range<T>, set<T>> ||
+                           is_same_v<Range<T>, unordered_multiset<T>> ||
+                           is_same_v<Range<T>, unordered_set<T>> ||
+                           is_same_v<Range<T>, vector<T>>>
+>
+ostream& operator<<(ostream& out, const Range<T>& range) {
+    constexpr auto brackets = is_same_v<Range<T>, vector<T>> ? "[]" : "{}";
+    out << brackets[0];
+    for (auto it = range.begin(); it != range.end(); ++it) {
+        out << *it << (next(it) != range.end() ? ", " : "");
+    }
+    return out << brackets[1];
+}
+
+template<
+    // Output stream overload for STL containers with two explicit type parameters.
+    template<class, class> class Range, class K, class V,
+    typename = enable_if_t<is_same_v<Range<K, V>, map<K, V>> ||
+                           is_same_v<Range<K, V>, multimap<K, V>> ||
+                           is_same_v<Range<K, V>, unordered_map<K, V>> ||
+                           is_same_v<Range<K, V>, unordered_multimap<K, V>>>
+>
+ostream& operator<<(ostream& out, const Range<K, V>& range) {
+    out << '{';
+    for (auto it = range.begin(); it != range.end(); ++it) {
+        out << it->first << ": " << it->second << (next(it) != range.end() ? ", " : "");
+    }
+    return out << '}';
+}
+
+template <class F, class S>
+ostream& operator<<(ostream& out, const pair<F, S>& duo) {
+  return out << '(' << duo.first << ", " << duo.second << ')';
+}
+
+// Hash Template Specializations
+template<class T, class U>
+struct hash<std::pair<T, U>> {
+    size_t operator()(const std::pair<T, U>& pair) const noexcept {
+        return std::hash<T>{}(pair.first) ^ (std::hash<U>{}(pair.second) << 1);
+    }
+};
+
+void solution();
+
+// Initalize the execution environment and call the solution function.
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    solution();
+}
+
+// -----------------------------------------------------------------------------
+
+// Splits |text| using |sep| as the delimeter string; empty tokens are omitted from the returned vector.
+vector<string> split(const string& text, const string&& sep) {
+    auto prev = text.begin();
+    auto next = text.begin();
+    vector<string> tokens;
+    for (bool first = true; next != text.end(); first = false) {
+        prev = first ? text.begin() : next + sep.size();
+        next = search(prev, text.end(), sep.begin(), sep.end());
+        if (prev != next) {
+            tokens.emplace_back(prev, next);
+        }
+    }
+    return tokens;
+}
+
+// -----------------------------------------------------------------------------
+
+void solution() {
+    string line;
+    getline(cin, line);
+    int N = stoi(line);
+    unordered_map<string, vector<pair<int, string>>> bags;
+    for (int i = 0; i < N; ++i) {
+        getline(cin, line);
+        const auto tokens = split(line, " ");
+        const auto outsider = tokens[0] + " " + tokens[1];
+        if (tokens[4] == "no") {
+            // The current bag contains no other bags.
+            continue;
+        }
+        for (int i = 4; i < tokens.size(); i += 4) {
+            const int quantity = stoi(tokens[i]);
+            const auto insider = tokens[i + 1] + " " + tokens[i + 2];
+            bags[outsider].emplace_back(quantity, insider);
+        }
+    }
+    const function<int(const string&)> explore = [&](const string& outsider) {
+        int counter = 1;
+        for (const auto& [quantity, insider] : bags[outsider]) {
+            counter += quantity * explore(insider);
+        }
+        return counter;
+    };
+    int insiders = explore("shiny gold") - 1;
+    cout << insiders << '\n';
+}
