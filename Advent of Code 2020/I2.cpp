7bd476e9bf42f85146897476c5cdd39892afbe19
Mandrenkov <M.Andrenkov@gmail.com>
2020-12-09 10:53:37 -0500

    Added solutions to Advent of Code 2020 Day 9 (I).

---
+++ Advent of Code 2020/I2.cpp
@@ -0,0 +1,112 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Macros
+#ifdef D3BUG
+    #define cerrd if (D3BUG) cerr
+#else
+    #define cerrd if (false) cerr
+#endif
+
+// Types
+template <typename T> using vector1D = vector<T>;
+template <typename T> using vector2D = vector<vector1D<T>>;
+template <typename T> using vector3D = vector<vector2D<T>>;
+
+template<
+    // Output stream overload for STL containers with one explicit type parameter.
+    template<class> class Range, class T,
+    typename = enable_if_t<is_same_v<Range<T>, deque<T>> ||
+                           is_same_v<Range<T>, forward_list<T>> ||
+                           is_same_v<Range<T>, list<T>> ||
+                           is_same_v<Range<T>, multiset<T>> ||
+                           is_same_v<Range<T>, set<T>> ||
+                           is_same_v<Range<T>, unordered_multiset<T>> ||
+                           is_same_v<Range<T>, unordered_set<T>> ||
+                           is_same_v<Range<T>, vector<T>>>
+>
+ostream& operator<<(ostream& out, const Range<T>& range) {
+    constexpr auto brackets = is_same_v<Range<T>, vector<T>> ? "[]" : "{}";
+    out << brackets[0];
+    for (auto it = range.begin(); it != range.end(); ++it) {
+        out << *it << (next(it) != range.end() ? ", " : "");
+    }
+    return out << brackets[1];
+}
+
+template<
+    // Output stream overload for STL containers with two explicit type parameters.
+    template<class, class> class Range, class K, class V,
+    typename = enable_if_t<is_same_v<Range<K, V>, map<K, V>> ||
+                           is_same_v<Range<K, V>, multimap<K, V>> ||
+                           is_same_v<Range<K, V>, unordered_map<K, V>> ||
+                           is_same_v<Range<K, V>, unordered_multimap<K, V>>>
+>
+ostream& operator<<(ostream& out, const Range<K, V>& range) {
+    out << '{';
+    for (auto it = range.begin(); it != range.end(); ++it) {
+        out << it->first << ": " << it->second << (next(it) != range.end() ? ", " : "");
+    }
+    return out << '}';
+}
+
+template <class F, class S>
+ostream& operator<<(ostream& out, const pair<F, S>& duo) {
+  return out << '(' << duo.first << ", " << duo.second << ')';
+}
+
+// Hash Template Specializations
+template<class T, class U>
+struct hash<std::pair<T, U>> {
+    size_t operator()(const std::pair<T, U>& pair) const noexcept {
+        return std::hash<T>{}(pair.first) ^ (std::hash<U>{}(pair.second) << 1);
+    }
+};
+
+void solution();
+
+// Initalize the execution environment and call the solution function.
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    solution();
+}
+
+// -----------------------------------------------------------------------------
+
+void solution() {
+    int N, P;
+    cin >> N >> P;
+    vector<int> data(N);
+    for (int i = 0; i < P + N; ++i) {
+        cin >> data[i];
+    }
+    optional<int> target = nullopt;
+    for (int i = P; i < P + N && !target; ++i) {
+        bool valid = false;
+        for (int j = i - P; j < i && !valid; ++j) {
+            for (int k = j + 1; k < i && !valid; ++k) {
+                valid = data[j] + data[k] == data[i];
+            }
+        }
+        if (!valid) {
+            target = data[i];
+        }
+    }
+    int beg = 0;
+    int end = 0;
+    int sum = 0;
+    while (sum < *target) {
+        sum += data[end++];
+    }
+    while (sum != *target) {
+        sum -= data[beg++];
+        while (sum < *target) {
+            sum += data[end++];
+        }
+    }
+    const auto [min_it, max_it] = minmax_element(data.begin() + beg, data.begin() + end);
+    int weakness = *min_it + *max_it;
+    cout << weakness << '\n';
+}
