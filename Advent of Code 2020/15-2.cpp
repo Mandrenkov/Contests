ce2fdb3147cc784f50d872989d8708f74d327b60
Mandrenkov <M.Andrenkov@gmail.com>
2020-12-15 18:49:28 -0500

    Added solutions to Advent of Code 2020 Day 15.

---
+++ Advent of Code 2020/15-2.cpp
@@ -0,0 +1,101 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Macros
+#ifdef D3BUG
+    #define cerrd if (D3BUG) cerr
+#else
+    #define cerrd if (false) cerr
+#endif
+
+// Types
+template <typename T> using vector1D = vector<T>;
+template <typename T> using vector2D = vector<vector1D<T>>;
+template <typename T> using vector3D = vector<vector2D<T>>;
+
+template<
+    // Output stream overload for STL containers with one explicit type parameter.
+    template<class> class Range, class T,
+    typename = enable_if_t<is_same_v<Range<T>, deque<T>> ||
+                           is_same_v<Range<T>, forward_list<T>> ||
+                           is_same_v<Range<T>, list<T>> ||
+                           is_same_v<Range<T>, multiset<T>> ||
+                           is_same_v<Range<T>, set<T>> ||
+                           is_same_v<Range<T>, unordered_multiset<T>> ||
+                           is_same_v<Range<T>, unordered_set<T>> ||
+                           is_same_v<Range<T>, vector<T>>>
+>
+ostream& operator<<(ostream& out, const Range<T>& range) {
+    constexpr auto brackets = is_same_v<Range<T>, vector<T>> ? "[]" : "{}";
+    out << brackets[0];
+    for (auto it = range.begin(); it != range.end(); ++it) {
+        out << *it << (next(it) != range.end() ? ", " : "");
+    }
+    return out << brackets[1];
+}
+
+template<
+    // Output stream overload for STL containers with two explicit type parameters.
+    template<class, class> class Range, class K, class V,
+    typename = enable_if_t<is_same_v<Range<K, V>, map<K, V>> ||
+                           is_same_v<Range<K, V>, multimap<K, V>> ||
+                           is_same_v<Range<K, V>, unordered_map<K, V>> ||
+                           is_same_v<Range<K, V>, unordered_multimap<K, V>>>
+>
+ostream& operator<<(ostream& out, const Range<K, V>& range) {
+    out << '{';
+    for (auto it = range.begin(); it != range.end(); ++it) {
+        out << it->first << ": " << it->second << (next(it) != range.end() ? ", " : "");
+    }
+    return out << '}';
+}
+
+template <class F, class S>
+ostream& operator<<(ostream& out, const pair<F, S>& duo) {
+  return out << '(' << duo.first << ", " << duo.second << ')';
+}
+
+// Hash Template Specializations
+template<class T, class U>
+struct hash<std::pair<T, U>> {
+    size_t operator()(const std::pair<T, U>& pair) const noexcept {
+        return std::hash<T>{}(pair.first) ^ (std::hash<U>{}(pair.second) << 1);
+    }
+};
+
+void solution();
+
+// Initalize the execution environment and call the solution function.
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    solution();
+}
+
+// -----------------------------------------------------------------------------
+
+void solution() {
+    int N;
+    cin >> N;
+    int last;
+    unordered_map<int, queue<int>> spoken;
+    for (int turn = 1; turn <= N; ++turn) {
+        cin >> last;
+        spoken[last].push(turn);
+    }
+    for (int turn = N + 1; turn <= 30000000; ++turn) {
+        if (spoken[last].size() < 2) {
+            // This number was spoken at most once.
+            last = 0;
+        } else {
+            // This number was spoken at least twice.
+            last = spoken[last].back() - spoken[last].front();
+        }
+        spoken[last].push(turn);
+        if (spoken[last].size() > 2) {
+            spoken[last].pop();
+        }
+    }
+    cout << last << '\n';
+}
