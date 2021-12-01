fdde1eac5fafb15482404ab1d8ffc550ccc6408e
Mandrenkov <M.Andrenkov@gmail.com>
2021-12-01 18:07:53 -0500

    Create CLI application to run AoC 2021 solutions

---
+++ Advent of Code 2021/src/main.rs
@@ -0,0 +1,48 @@
+use clap::{load_yaml, App};
+use colored::*;
+use std::fs;
+
+mod d01;
+
+fn main() {
+    let yaml = load_yaml!("cli.yaml");
+    let matches = App::from(yaml).get_matches();
+
+    let day = parse_integer_option(&matches, "DAY", 1, 25);
+    let part = parse_integer_option(&matches, "PART", 1, 2);
+
+    println!(
+        "{} / {}",
+        format!("Day {:02}", day).green(),
+        format!("Part {}", part).red()
+    );
+    println!("{}", "-".repeat(80));
+
+    let filename = format!("input/d{:02}p{}.txt", day, part);
+    let input = fs::read_to_string(filename).expect("Failed to read input file");
+
+    match day {
+        1 => match part {
+            1 => d01::p1(input),
+            _ => d01::p2(input),
+        },
+        _ => {
+            eprintln!("No solution is available yet for day {}.", day)
+        }
+    }
+}
+
+/// Parses the given CLI option into an integer.
+fn parse_integer_option(matches: &clap::ArgMatches, option: &str, min: i32, max: i32) -> i32 {
+    if let Some(string) = matches.value_of(option) {
+        let value = string.parse().unwrap();
+        if value < min || value > max {
+            panic!(
+                "The value of option '{}' does not fall in the range [{}, {}].",
+                option, min, max
+            );
+        }
+        return value;
+    }
+    panic!("The value of option '{}' could not be retrieved.", option);
+}
