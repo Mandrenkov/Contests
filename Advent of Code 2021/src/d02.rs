992785003a30abe87b169ffeaa6a211c18755f07
Mandrenkov <M.Andrenkov@gmail.com>
2021-12-02 21:08:13 -0500

    Add solution for Day 02 Part 2

--- Advent of Code 2021/src/d02.rs
+++ Advent of Code 2021/src/d02.rs
@@ -2,15 +2,13 @@
 pub fn p1(input: String) {
     let tokens = input.split_whitespace();
 
-    let cmd_it = tokens.clone().step_by(2);
-    let val_it = tokens
-        .clone()
-        .skip(1)
-        .step_by(2)
-        .map(|v| v.parse::<i32>().unwrap());
+    let str_to_int = |token: &str| token.parse::<i64>().unwrap();
 
-    let mut x: i32 = 0;
-    let mut y: i32 = 0;
+    let cmd_it = tokens.clone().step_by(2);
+    let val_it = tokens.clone().skip(1).step_by(2).map(str_to_int);
+
+    let mut x: i64 = 0;
+    let mut y: i64 = 0;
 
     for (cmd, val) in cmd_it.zip(val_it) {
         match cmd {
@@ -24,4 +22,28 @@ pub fn p1(input: String) {
 }
 
 /// Solves the Day 02 Part 2 puzzle with respect to the given input.
-pub fn p2(_input: String) {}
+pub fn p2(input: String) {
+    let tokens = input.split_whitespace();
+
+    let str_to_int = |token: &str| token.parse::<i64>().unwrap();
+
+    let cmd_it = tokens.clone().step_by(2);
+    let val_it = tokens.clone().skip(1).step_by(2).map(str_to_int);
+
+    let mut x: i64 = 0;
+    let mut y: i64 = 0;
+    let mut aim: i64 = 0;
+
+    for (cmd, val) in cmd_it.zip(val_it) {
+        match cmd {
+            "up" => aim -= val,
+            "down" => aim += val,
+            _ => {
+                x += val;
+                y += aim * val;
+            },
+        }
+    }
+
+    println!("{}", x * y);
+}
