43580a59f2174c32b574b93f56f56424383ff367
Mikhail Andrenkov <M.Andrenkov@gmail.com>
2022-12-01 16:15:12 -0500

    Add solution for day 01

---
+++ Advent of Code 2022/day_01_solution.ts
@@ -0,0 +1,13 @@
+import { readFileSync } from "fs";
+
+const path = "day_01_unique.txt";
+const input = readFileSync(path, "utf8");
+const lines = input.split("\n\n")
+
+const add = (acc: number, cur: number) => acc + cur;
+const sub = (lhs: number, rhs: number) => lhs - rhs;
+
+const elves = lines.map(line => line.trim().split("\n").map((cal) => +cal).reduce(add)).sort(sub);
+
+console.log("Answer to Part 1:", elves.slice(-1).reduce(add));
+console.log("Answer to Part 2:", elves.slice(-3).reduce(add));
